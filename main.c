da91fec1dd0d0c9bfab18bac7644db8c9a1dd494
Andree Fonseca <86313603+AndreeFonseca@users.noreply.github.com>
2024-01-17 23:21:01 -0500

    Update main.c
    cambio codigo main

--- main.c
+++ main.c
@@ -1,38 +1,23 @@
 #include "funciones.h"
 
-STATE estado;
-EVENT evento;
+tLight estado;
+tState evento;
+
 
 int main(int argc, char const *argv[])
 {
-  
-  estado=RED;
-  evento=NO_EVENT;
 
-  while(estado){
-    
-    switch(estado){
-      case RED:
-        luz_roja();
-        if(evento == GO_COMMAND)
-        	estado = GREEN;
-        break;
-      case GREEN:
-        luz_verde();
-        if(evento == STOP_COMMAND)
-        	estado = YELLOW;
-        break;
-      case YELLOW:
-        luz_amarilla();
-        if(evento == TIMER_TIMEOUT)
-        	estado = RED;
-        break;
-      default:
-        estado = EXIT;
-    }
+  estado=kRedLight;
+  evento=kRedState;
+  char c = getchar();
+  
+  while(c != 'q'){
 
     /* letra q = SALIR, letra s = STOP_COMMAND, 
        letra t = TIMER_TIMEOUT, letra g = GO_COMMAND*/
-    evento = get_event();
+    //HandleEventGo(stateTable, estado, evento);
+    printf("hola");
   }
+
+
 }
