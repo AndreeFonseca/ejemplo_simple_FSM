567e74ad807e480b21092349309a449a9a732122
Andree Fonseca <86313603+AndreeFonseca@users.noreply.github.com>
2024-01-17 23:23:24 -0500

    Update funciones.h
    cambios en codigo de funciones.h

--- funciones.h
+++ funciones.h
@@ -1,12 +1,26 @@
-#include <unistd.h>
 #include <fcntl.h>
-#include <stdio.h>
 #include <pthread.h>
+#include <stdio.h>
+#include <unistd.h>
 
-typedef enum {EXIT = 0, RED, GREEN, YELLOW} STATE;
-typedef enum {NO_EVENT = 0, STOP_COMMAND, GO_COMMAND, TIMER_TIMEOUT} EVENT;
+typedef enum { kRedLight = 0, kYellowLight = 1, kGreenLight = 2 } tLight;
+typedef enum { kRedState = 0, kYellowState = 1, kGreenState = 2 } tState;
 
-int get_event();
+struct sStateTableEntry {
+  tLight light;
+  tState goEvent;
+  tState stopEvent;
+  tState timeoutEvent;
+};
+
+struct sStateTableEntry stateTable[] = {
+    {kRedLight, kGreenState, kRedState, kRedState},        // Rojo
+    {kYellowLight, kYellowState, kYellowState, kRedState}, // Amarillo
+    {kGreenLight, kGreenState, kYellowState, kGreenState}, // Verde
+};
+
 void luz_verde();
 void luz_roja();
 void luz_amarilla();
+void HandleEventGo();
+void lightOn();
